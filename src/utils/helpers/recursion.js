013d3262cf9c8432e7abda4367e3776d5a0a44ed
parth <srivastavaparth2016@gmail.com>
2018-03-06 11:51:02 +0530

    changes in the recursion implementation

--- src/utils/helpers/recursion.js
+++ src/utils/helpers/recursion.js
@@ -2,17 +2,17 @@ const Models = require('../../../models');
 const createShort = require('../../utils/helpers/createShort');
 
 const recursion = (longUrl, shortUrl, start, end) => {
-  shortUrl = shortUrl.slice(start, end);
-  return Models.urls.createObject(longUrl, shortUrl).spread((url, created) => {
+  const shortUrlnew = shortUrl.slice(start, end);
+  return Models.urls.createObject(longUrl, shortUrlnew).spread((url, created) => {
     console.log('hi');
     if (created === false) {
       if (longUrl !== url.longUrl) {
-        return recursion(longUrl, start + 1, end + 1);
+        return recursion(longUrl, shortUrl, start + 1, end + 1);
       }
-      return shortUrl;
+      return shortUrlnew;
     }
 
-    return shortUrl;
+    return shortUrlnew;
   });
 };
 
