6e579de966d005341e2baede3d1a3e692696a982
parth <srivastavaparth2016@gmail.com>
2018-03-08 12:39:50 +0530

    creating redis client on the server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const Hapi = require('hapi');
 const routes = require('./routes');
 const Good = require('good');
+const redis = require('redis');
 
 
 const server = new Hapi.Server();
+const client = redis.createClient();
+client.on('error', (err) => {
+  console.log(`Error ${err}`);
+});
 server.connection({
   host: 'localhost',
   port: Number(8080),
@@ -30,7 +35,7 @@ server.register({
   }
 });
 
-server.route(routes);
+server.route(routes(client));
 if (!module.parent) {
   server.start((err) => {
     console.log(err);
